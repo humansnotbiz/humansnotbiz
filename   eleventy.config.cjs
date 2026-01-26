80ed94c2d5d0e56e73b6a3760c526f71917dd008
oana <humansnotbiz@gmail.com>
2026-01-26 19:07:38 +0100

    Added project structure

---
+++   eleventy.config.cjs
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({ "assets": "assets" });
+
+    return {
+        dir: {
+            input: ".",
+            includes: "_includes",
+            output: "_site"
+        },
+        templateFormats: ["njk", "md", "html"],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk"
+    };
+};
